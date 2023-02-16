b40f83796d22ff987e89965bf1ade03a2b3c4f98
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2023-02-15 20:37:41 -0700

    Update package names to match layout

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 
 	"github.com/NOAA-GSL/vxDataProcessor/pkg/api"
-
 	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Println(service.TestString())
+	fmt.Println(api.TestString())
 	fmt.Println(quote.Go())
 }

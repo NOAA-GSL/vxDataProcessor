b5e24333878fd770b1d0efcc46c652e016cb7c17
randy.pierce <randy.pierce@noaa.gov>
2023-02-23 13:35:24 -0700

    initial

--- go.mod
+++ go.mod
@@ -5,6 +5,18 @@ go 1.19
 require rsc.io/quote v1.5.2
 
 require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
+
+require (
+	github.com/aclements/go-moremath v0.0.0-20210112150236-f10218a38794
+	github.com/go-playground/validator/v10 v10.11.2
+	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/tools v0.6.0
+	rsc.io/benchstat v0.0.0-20180218003226-ca9dcc07a32d
 	rsc.io/sampler v1.3.0 // indirect
 )
---
+++ pkg/builder/iBuilder.go
@@ -0,0 +1,33 @@
+package builder
+/*
+please refer to https://gist.github.com/vaskoz/10073335
+*/
+
+type DerivedData struct {
+	CtlPop []float64
+	ExpPop []float64
+}
+type InputData struct {
+	Rows []struct {
+		InputData []DerivedData
+	}
+}
+// -1 or 1
+type GoodnessPolarity int
+type Threshold float64
+type builderResult struct {
+	data DerivedData
+	goodnessPolarity GoodnessPolarity
+	majorThreshold Threshold
+	minorThreshold Threshold
+	value int
+}
+
+type Builder interface {
+	deriveData(InputData) Builder
+	setGoodnessPolarity(GoodnessPolarity) Builder
+	setMajorThreshold(Threshold) Builder
+	setMinorThreshold(Threshold) Builder
+	computeSignificance(DerivedData) Builder
+	Build() Builder
+}

151bbf2638a972c2127b2751febd10ff00d3c64d
randy.pierce <randy.pierce@noaa.gov>
2023-02-25 19:15:59 -0700

    playing with lint

--- pkg/director/director.go
+++ pkg/director/director.go
@@ -5,14 +5,14 @@ import (
 	"github.com/NOAA-GSL/vxDataProcessor/pkg/builder"
 )
 var gp builder.GoodnessPolarity = 1
-// var minorThreshold builder.Threshold = 0.05
-// var majorThreshold builder.Threshold = 0.01
+var minorThreshold builder.Threshold = 0.05
+var majorThreshold builder.Threshold = 0.01
 var ip builder.InputData
-cellBuilder := builder.GetBuilder("TwoSampleTTest")
+var cellBuilder ScorecardCellBuilder = builder.GetBuilder("TwoSampleTTest")
 
 cellBuilder.SetGoodnessPolarity(gp)
-cellBuilder.SetMinorThreshold(0.05)
-cellBuilder.SetMajorThreshold(0.01)
+cellBuilder.SetMinorThreshold(minorThreshold)
+cellBuilder.SetMajorThreshold(majorThreshold)
 cellBuilder.DeriveData(ip)
 cell := cellBuilder.GetScorecardCell()
 cellBuilder.ComputeSignificance(cell.Data)

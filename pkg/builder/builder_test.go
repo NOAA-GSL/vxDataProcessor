535147a841603ad945b820309ba23b5d39e54b5b
randy.pierce <randy.pierce@noaa.gov>
2023-02-28 10:28:29 -0700

    tests working but not all are passing

--- pkg/builder/builder_test.go
+++ pkg/builder/builder_test.go
@@ -3,27 +3,70 @@ package builder_test
 import (
 	"github.com/NOAA-GSL/vxDataProcessor/pkg/builder"
 	"testing"
+	"fmt"
 )
 
-func TestTwoSampleTTestBuilder(t *testing.T) {
-	var gp builder.GoodnessPolarity = 1
-	// var minorThreshold builder.Threshold = 0.05
-	// var majorThreshold builder.Threshold = 0.01
-	var ip builder.InputData
-	cellBuilder := builder.GetBuilder("TwoSampleTTest")
+var gp  = builder.GoodnessPolarity(1)
+var minorThreshold = builder.Threshold(0.05)
+var majorThreshold = builder.Threshold(0.01)
+var cellPtr = builder.GetBuilder("TwoSampleTTest")
 
-	cellBuilder.SetGoodnessPolarity(gp)
-	cellBuilder.SetMinorThreshold(0.05)
-	cellBuilder.SetMajorThreshold(0.01)
-	cellBuilder.DeriveData(ip)
-	cell := cellBuilder.GetScorecardCell()
-	cellBuilder.ComputeSignificance(cell.Data)
-	value := cell.Value
-	if value != 1 {
-		t.Fatal("Wrong value :")
+func TestTwoSampleTTestBuilder_test_identical(t *testing.T) {
+	cellPtr.SetGoodnessPolarity(gp)
+	cellPtr.SetMinorThreshold(minorThreshold)
+	cellPtr.SetMajorThreshold(majorThreshold)
+	cellPtr.SetInputData(builder.DerivedDataElement{
+			CtlPop: []float64{0.2,1.3,3.2,4.5},
+			ExpPop: []float64{0.2,1.3,3.2,4.5},
+	})
+	cellPtr.ComputeSignificance(cellPtr.Data)
+	fmt.Println("Pval is", cellPtr.StatValue, "value is ", cellPtr.Value)
+	if cellPtr.Value != 0 {
+		t.Fatal("test_indentical_wrong value :", cellPtr.Value)
+	}
+}
+func TestTwoSampleTTestBuilder_test_2(t *testing.T) {
+	cellPtr.SetInputData(builder.DerivedDataElement{
+		CtlPop: []float64{1.0,1.1,1.2,1.09,1.08},
+		ExpPop: []float64{0.99,1.09,1.19,1.08,1.07},
+	})
+	cellPtr.ComputeSignificance(cellPtr.Data)
+	fmt.Println("Pval is", cellPtr.StatValue, "value is ", cellPtr.Value)
+	if cellPtr.Value != 0 {
+		t.Fatal("test_2_wrong value :", cellPtr.Value)
+	}
+}
+func TestTwoSampleTTestBuilder_test_1(t *testing.T) {
+		cellPtr.SetInputData(builder.DerivedDataElement{
+		CtlPop: []float64{0.2,0.31,0.11,0},
+		ExpPop: []float64{0.2,0.3,0.1,0.9},
+	})
+	cellPtr.ComputeSignificance(cellPtr.Data)
+	fmt.Println("Pval is", cellPtr.StatValue, "value is ", cellPtr.Value)
+	if cellPtr.Value != 0 {
+		t.Fatal("test_1 wrong value :", cellPtr.Value)
+	}
+}
+func TestTwoSampleTTestBuilder_test_0(t *testing.T) {
+	cellPtr.SetInputData(builder.DerivedDataElement{
+		CtlPop: []float64{0.2,0.303,0.101,0},
+		ExpPop: []float64{0.2,0.3,0.1,0.01},
+	})
+	cellPtr.ComputeSignificance(cellPtr.Data)
+	fmt.Println("Pval is", cellPtr.StatValue, "value is ", cellPtr.Value)
+	if cellPtr.Value != 0 {
+		t.Fatal("test_0 wrong value :", cellPtr.Value)
 	}
 }
 
-func NewTwoSampleTTestBuilder() {
-	panic("unimplemented")
+func TestTwoSampleTTestBuilder_different_lengths(t *testing.T) {
+	cellPtr.SetInputData(builder.DerivedDataElement{
+		CtlPop: []float64{0.2,0.303,0.101,0},
+		ExpPop: []float64{0.2,0.3,0.1},
+	})
+	cellPtr.ComputeSignificance(cellPtr.Data)
+	fmt.Println("Pval is", cellPtr.StatValue, "value is ", cellPtr.Value)
+	if cellPtr.Value != 0 {
+		t.Fatal("Wrong value :", cellPtr.Value)
+	}
 }

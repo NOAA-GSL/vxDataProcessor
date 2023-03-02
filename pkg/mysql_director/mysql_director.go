ff0926ed30d2d757fbf2fbcbe3960e644796bde7
randy.pierce <randy.pierce@noaa.gov>
2023-03-02 12:49:54 -0700

    implementing builder

--- pkg/mysql_director/mysql_director.go
+++ pkg/mysql_director/mysql_director.go
@@ -2,6 +2,7 @@ package mysql_director
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -37,17 +38,23 @@ var inputData = builder.DerivedDataElement{
 	ExpPop: []float64{0.1, 1.5, 3.0, 4.1},
 }
 
-func TestString() string {
-	return "this is a string from mysql_director"
+func checkFileExists(filePath string) bool {
+	_, error := os.Stat(filePath)
+	return !errors.Is(error, os.ErrNotExist)
 }
 
-func getCredentials() *CB_credentials {
+func GetCredentials() (*CB_credentials, error) {
 	var cb_credentials  = CB_credentials{}
 	cb_credentials.cb_scope = "_default"
 	var filename = os.Getenv("HOME") + strconv.QuoteRune(os.PathSeparator) + "adb-cb4-credentials"
+	if ! checkFileExists(filename) {
+		log.Print(fmt.Sprint("mysql_director  - credential does not exist - ", filename))
+		return nil, errors.New(fmt.Sprint("mysql_director error credential file does not exist", filename))
+	}
 	file, err := os.Open(filename)
 	if err != nil {
-		log.Fatal(fmt.Sprint("director - credential file open error - ", err))
+		log.Print(fmt.Sprint("mysql_director - credential file open error - ", err))
+		return nil, errors.New(fmt.Sprint("mysql_director error opening credential file", err))
 	}
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
@@ -72,15 +79,35 @@ func getCredentials() *CB_credentials {
 		case "cb_bucket":
 			cb_credentials.cb_bucket = s[1]
 		case "cb_collection":
-			cb_credentials.cb_collection = s[1]
+			// for scorecards the collection is always 'SCORECARD'
+			cb_credentials.cb_collection = "SCORECARD"
 		default: // do nothing
 		}
 	}
-	return &cb_credentials
+	if cb_credentials.cb_host == "" {
+        return nil, errors.New(fmt.Sprint("mysql_director cb_credentials.cb_host has not been set", filename))
+    }
+	if cb_credentials.cb_user == "" {
+        return nil, errors.New(fmt.Sprint("mysql_director cb_credentials.cb_user has not been set", filename))
+    }
+	if cb_credentials.cb_password == "" {
+        return nil, errors.New(fmt.Sprint("mysql_director cb_credentials.cb_password has not been set", filename))
+    }
+	if cb_credentials.cb_bucket == "" {
+        return nil, errors.New(fmt.Sprint("mysql_director cb_credentials.cb_bucket has not been set", filename))
+    }
+	if cb_credentials.cb_collection == "" {
+        return nil, errors.New(fmt.Sprint("mysql_director cb_credentials.cb_collection has not been set", filename))
+    }
+	return &cb_credentials, nil
 }
 
-func GetConnection() *CB_connection {
-	var cb_credentials *CB_credentials = getCredentials()
+func GetConnection() (*CB_connection, error) {
+	var cb_credentials, err = GetCredentials()
+	if err != nil {
+        return nil, errors.New(fmt.Sprint("mysql_director GetCredentials error ", err))
+    }
+
 	var cb_connection CB_connection
 	var options = gocb.ClusterOptions{
 		Authenticator: gocb.PasswordAuthenticator{
@@ -88,60 +115,66 @@ func GetConnection() *CB_connection {
 			Password: cb_credentials.cb_password,
 		},
 	}
-	if err := options.ApplyProfile(gocb.ClusterConfigProfileWanDevelopment); err != nil {
-		log.Fatal(err)
+	if err = options.ApplyProfile(gocb.ClusterConfigProfileWanDevelopment); err != nil {
+        return nil, errors.New(fmt.Sprint("mysql_director gocb ApplyProfile error ", err))
 	}
 	// Initialize the Connection
-	cluster, err := gocb.Connect("couchbases://"+ cb_credentials.cb_host, options)
+	var cluster *gocb.Cluster
+	cluster, err = gocb.Connect("couchbases://"+ cb_credentials.cb_host, options)
 	if err != nil {
-		log.Fatal(err)
+        return nil, errors.New(fmt.Sprint("mysql_director gocb Connect error ", err))
 	}
 	cb_connection.CB_cluster = cluster
 	cb_connection.CB_bucket = cluster.Bucket(cb_credentials.cb_bucket)
 	err = cb_connection.CB_bucket.WaitUntilReady(5*time.Second, nil)
 	if err != nil {
-		log.Fatal(err)
+        return nil, errors.New(fmt.Sprint("mysql_director CB_bucket.WaitUntilReady error ", err))
 	}
 	cb_connection.CB_scope = cb_connection.CB_bucket.Scope(cb_credentials.cb_scope)
 	col := cb_connection.CB_scope.Collection(cb_credentials.cb_collection)
 	cb_connection.CB_collection = col
-	return &cb_connection
+	return &cb_connection, nil
 }
 
-func Build(documentId string) {
+func Build(documentId string) error {
 	// get the connection
-	var cb_connection = GetConnection()
+	var cb_connection, err = GetConnection()
+	if err != nil {
+        return errors.New(fmt.Sprint("mysql_director Build GetConnection error ", err))
+	}
+
 	// get the scorecard document
 	var docOut *gocb.GetResult
-	var err error
 	docOut, err = cb_connection.CB_collection.Get(documentId, nil)
 	if err != nil {
-		log.Fatal(err)
+        return errors.New(fmt.Sprint("mysql_director Build GetResult error ", err))
 	}
 	fmt.Print(docOut)
-	// builde the input data elements and
+	
+	// build the input data elements and
 	// for all the input elements fire off a thread to do the compute
 	var cellPtr = builder.GetBuilder("TwoSampleTTest")
 	err = cellPtr.SetGoodnessPolarity(gp)
 	if err != nil {
-		log.Fatal(fmt.Sprint("director - build - SetGoodnessPolarity - error message : ", err))
+		return errors.New(fmt.Sprint("mysql_director Build SetGoodnessPolarity error ", err))
 	}
 	err = cellPtr.SetMinorThreshold(minorThreshold)
 	if err != nil {
-		log.Fatal(fmt.Sprint("director - build - SetMinorThreshold - error message : ", err))
+		return errors.New(fmt.Sprint("mysql_director - build - SetMinorThreshold - error message : ", err))
 	}
 	err = cellPtr.SetMajorThreshold(majorThreshold)
 	if err != nil {
-		log.Fatal(fmt.Sprint("director - build - SetMajorThreshold - error message : ", err))
+		return errors.New(fmt.Sprint("mysql_director - build - SetMajorThreshold - error message : ", err))
 	}
 	err = cellPtr.SetInputData(inputData)
 	if err != nil {
-		log.Fatal(fmt.Sprint("director - build - SetInputData - error message : ", err))
+		return errors.New(fmt.Sprint("mysql_director - build - SetInputData - error message : ", err))
 	}
 	err = cellPtr.ComputeSignificance(cellPtr.Data)
 	if err != nil {
-		log.Fatal(fmt.Sprint("director - build - ComputeSignificance - error message : ", err))
+		return errors.New(fmt.Sprint("mysql_director - build - ComputeSignificance - error message : ", err))
 	}
 	// insert the elements into the in-memory document
 	// upsert the document
+	return nil
 }

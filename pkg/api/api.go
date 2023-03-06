d0e2fbd351bd09e5e2c448fe1a0500fab574796d
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2023-03-06 16:27:31 -0700

    Initial commit of API server and handler functions

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,5 +1,24 @@
 package api
 
-func TestString() string {
-	return "this is a string from api"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Start() {
+	router := gin.Default()
+	server := NewJobServer()
+
+	router.POST("/jobs/", server.createJobHandler)
+	router.GET("/jobs/", server.getAllJobsHandler)
+	router.GET("/jobs/:id", server.getJobHandler)
+
+	// healthcheck
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080
 }

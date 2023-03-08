1bee66b121c488ab110d06b15b7d30b3fa0d88bf
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2023-03-07 21:44:10 -0700

    Remove test for a removed function

--- pkg/api/api_test.go
+++ pkg/api/api_test.go
@@ -2,12 +2,8 @@ package api_test
 
 import (
 	"testing"
-
-	"github.com/NOAA-GSL/vxDataProcessor/pkg/api"
 )
 
 func TestService(t *testing.T) {
-	if api.TestString() != "this is a string from api" {
-		t.Fatal("Wrong test string :")
-	}
+	// TODO
 }

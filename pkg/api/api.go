eb6bd5130b52eac7d1e30e7e6793a924b980065d
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2023-03-16 20:45:57 -0600

    Add worker and dispatch methods to the API
    The Dispatch method pulls jobs out of the store and sends them to the
    Worker for processing. Currently the Worker just prints the DocID.
    However, it will eventually call a manager.

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/NOAA-GSL/vxDataProcessor/pkg/api/jobstore"
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,3 +26,38 @@ func SetupRouter(js *jobstore.JobStore) *gin.Engine {
 
 	return router
 }
+
+func Worker(id int, jobs <-chan jobstore.Job, status chan<- string) {
+	for {
+		job := <-jobs // block until we get a job
+		fmt.Println("worker", id, "started  docID", job.DocID)
+		// status <- "processing" // We'll need a way to associate these with a job
+
+		// Do work
+		fmt.Println("Worker", id, "processing docID", job.DocID)
+		time.Sleep(time.Second)
+
+		// report status
+		// status <- "finished"
+		fmt.Println("worker", id, "finished docID", job.DocID)
+	}
+}
+
+func Dispatch(jobChan chan<- jobstore.Job, js *jobstore.JobStore) {
+	for {
+		n := 2 // number of jobs to pull off the queue
+		jobs, err := js.GetJobsToProcess(n)
+		if err != nil {
+			if err.Error() == "No unprocessed jobs available" {
+				continue
+			}
+			panic(err)
+		}
+
+		for _, job := range jobs {
+			jobChan <- job
+			fmt.Printf("Dispatched item #%v\n", job)
+		}
+	}
+}
+

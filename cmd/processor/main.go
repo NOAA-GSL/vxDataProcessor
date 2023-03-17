93d657fcb9866b5834276ea93ff2b221bdd5183f
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2023-03-16 20:48:45 -0600

    Update main to dispatch and process Jobs
    Currently the "processing" is just printing out the DocID. However, this
    will be where we will call a data processing routine once those are
    ready.

--- cmd/processor/main.go
+++ cmd/processor/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"github.com/NOAA-GSL/vxDataProcessor/pkg/api"
+	"github.com/NOAA-GSL/vxDataProcessor/pkg/api/jobstore"
 )
 
 func main() {
-	router := api.SetupRouter()
+	jobs := make(chan jobstore.Job)
+	status := make(chan string) // this channel needs a status and jobID
+	js := jobstore.NewJobStore()
+
+	// setup dispatcher to send jobs from queue to worker pool
+	// https://webdevstation.com/posts/simple-queue-implementation-in-golang/
+	go api.Dispatch(jobs, js)
+
+	// setup worker pool to recieve jobs to pass on to process and to send status updates
+	for w := 1; w <= 5; w++ {
+		go api.Worker(w, jobs, status)
+	}
+
+	router := api.SetupRouter(js)
 
 	err := router.Run(":8080") // listen and serve on 0.0.0.0:8080
 	if err != nil {

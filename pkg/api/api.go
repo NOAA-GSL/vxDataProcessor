f303aa3168c53f191daa6132f3a9ae51e898b0f2
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2023-03-30 12:54:41 -0600

    Add a middleware function to instrument requests
    Inspired by the go-gin-prometheus middleware but simplified and updated
    for our usecase. The middleware will instrument all requests made to the
    API service and track the paths that were requested, http status code,
    request duration, as well as request & response size.

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -16,6 +16,7 @@ import (
 func SetupRouter(js *jobstore.JobStore) *gin.Engine {
 	router := gin.Default()
 	server := NewJobServer(js)
+	router.Use(prometheusMiddleware()) // attach our Prometheus middleware
 
 	router.POST("/jobs/", server.createJobHandler)
 	router.GET("/jobs/", server.getAllJobsHandler)
